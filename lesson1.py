ceab47eb74f2222f1d7a61917539c82cf9afd846
TatianaShuljenko <tatianashuljenko@gmail.com>
2020-06-06 23:00:19 +0300

    first fixes

---
+++ lesson1.py
@@ -0,0 +1,22 @@
+# first lesson
+poem='''I do not like thee, Doctor Fell.
+The reason why, I cannot tell.
+But this I know, and know full well:
+I do not like thee, Doctor Fell.'''
+print(poem)
+#1
+palindrome='A man.\nA plan.\nA canal:\nPanama.'
+print(palindrome)
+#2
+start='Na'* 4+ '\n'
+middle='Hey'*3+'\n'
+end='Goodbye.'
+print(start+start+middle+end)
+#3
+letters='akjbytdghhhkjkgtdgtrfhjbkjlkjgbhdcgfvh'
+print(letters[-1::-1])
+a=len(poem)
+print("the poem lengh is",str(a))
+#4
+todos='get gloves,get mask,give cat vitamins,call ambulance'
+print(todos.split(','))
